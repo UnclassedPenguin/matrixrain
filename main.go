4259011193dcd29c2aba9fef9852a2ac8e32b32d
Tyler <tvanden@live.com>
2022-12-17 14:49:33 -0700

    working

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
   "os"
   "fmt"
+  "time"
+  "math/rand"
   "github.com/gdamore/tcell/v2"
 )
 
 type Symbol struct {
   x int
   y int
-  value rune
+  value int
 }
 
-func (s Symbol) setToRandomSymbol() {
-
+func (s *Symbol) setToRandomSymbol() {
+  rand.Seed(time.Now().UnixNano())
+  //s.value = 0x30a0 + rand.Intn(96)
+  s.value = 12448 + rand.Intn(97)
 }
 
 // This is used just to write strings to the screen.
@@ -29,6 +33,8 @@ func main() {
     fmt.Println("Error in tcell.NewScreen:", err)
   }
 
+  //encoding.Register()
+
   if err := s.Init(); err != nil {
     fmt.Println("Error initializing screen:", err)
     os.Exit(1)
@@ -40,28 +46,35 @@ func main() {
 
   x, y := s.Size()
 
-  writeToScreen(s, style, x/2-4, y/2, "Welcome")
-
-  b := []byte{0x30, 0xA0}
-
-  writeToScreen(s, style, x/2-4, y/2+1, string(b))
-
-  for {
-  switch ev := s.PollEvent().(type) {
-    case *tcell.EventResize:
-      s.Sync()
-    case *tcell.EventKey:
-      switch ev.Key() {
-      case tcell.KeyCtrlC, tcell.KeyEscape:
-        s.Fini()
-        os.Exit(0)
-      case tcell.KeyRune:
-        switch ev.Rune() {
-        case 'q', 'Q':
+  go func() {
+    for {
+    switch ev := s.PollEvent().(type) {
+      case *tcell.EventResize:
+        s.Sync()
+      case *tcell.EventKey:
+        switch ev.Key() {
+        case tcell.KeyCtrlC, tcell.KeyEscape:
           s.Fini()
           os.Exit(0)
+        case tcell.KeyRune:
+          switch ev.Rune() {
+          case 'q', 'Q':
+            s.Fini()
+            os.Exit(0)
+          }
         }
       }
     }
+  }()
+
+  for {
+    var sym Symbol
+
+    sym.setToRandomSymbol()
+
+    writeToScreen(s, style, x/2-4, y/2+1, string(sym.value))
+
+    time.Sleep(time.Millisecond * 100)
   }
+
 }

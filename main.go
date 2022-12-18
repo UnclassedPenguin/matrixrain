453c26df733513698d2a6d5b7d68a41b599c8b33
Tyler <tvanden@live.com>
2022-12-17 17:01:43 -0700

    it "works" but not quite there yet

--- main.go
+++ main.go
@@ -8,33 +8,65 @@ import (
   "github.com/gdamore/tcell/v2"
 )
 
+// A symbol is an individual character
 type Symbol struct {
   x int
   y int
+  speed int
   value int
 }
 
-func (sym *Symbol) rain(y int) {
-  sym.y += 1
-  if sym.y > y+1 {
+// Rain causes the symbol to advance by its speed every "frame"
+// in the y direction
+func (sym *Symbol) rain(s tcell.Screen) {
+  _, y := s.Size()
+
+  sym.y += sym.speed
+  if sym.y > y {
     sym.y = 0
   }
 }
 
+// Picks a random Katakana character for symbol
 func (sym *Symbol) setToRandomSymbol() {
   rand.Seed(time.Now().UnixNano())
   sym.value = 0x30a0 + rand.Intn(96)
 }
 
-func (sym *Symbol) render(s tcell.Screen, style tcell.Style, speed int) {
+// Draws the symbol to the screen
+func (sym *Symbol) render(s tcell.Screen, style tcell.Style) {
   rand.Seed(time.Now().UnixNano())
   if rand.Intn(10) < 2 {
     sym.setToRandomSymbol()
   }
   writeToScreen(s, style, sym.x, sym.y, string(sym.value))
-  s.Sync()
-  time.Sleep(time.Millisecond * time.Duration(speed))
-  s.Clear()
+}
+
+// A stream is an array of symbols
+type Stream struct {
+  symbols []Symbol
+  totalSymbols int
+}
+
+// Creates the array of symbols
+func (stream *Stream) generateSymbols(x, y, speed int) {
+  for i := 0; i < stream.totalSymbols; i++ {
+    sym := Symbol{
+      x: x,
+      y: y-i,
+      speed: speed,
+    }
+    sym.setToRandomSymbol()
+    stream.symbols = append(stream.symbols, sym)
+  }
+}
+
+// Draws a stream to the screen
+func (stream *Stream) render(s tcell.Screen, style tcell.Style) {
+    for i := 0; i < stream.totalSymbols; i++ {
+      stream.symbols[i].render(s, style)
+      stream.symbols[i].rain(s)
+    }
 }
 
 // This is used just to write strings to the screen.
@@ -50,8 +82,6 @@ func main() {
     fmt.Println("Error in tcell.NewScreen:", err)
   }
 
-  //encoding.Register()
-
   if err := s.Init(); err != nil {
     fmt.Println("Error initializing screen:", err)
     os.Exit(1)
@@ -61,8 +91,7 @@ func main() {
 
   style := tcell.StyleDefault.Foreground(tcell.ColorGreen)
 
-  x, y := s.Size()
-
+  x, _ := s.Size()
 
   go func() {
     for {
@@ -85,17 +114,30 @@ func main() {
     }
   }()
 
-  var sym Symbol
-  sym = Symbol{
-    x: x/2,
-    y: y-y,
+  rand.Seed(time.Now().UnixNano())
+
+  var streams []Stream
+
+  for i := 0; i < x; i++ {
+    if i % 3 == 0 {
+      stream := Stream{
+        totalSymbols: rand.Intn(x/2),
+      }
+      speed := rand.Intn(2)+1
+      //speed := 1
+      rany := rand.Intn(50)
+      stream.generateSymbols(i, rany*-1, speed)
+      streams = append(streams, stream)
+    }
   }
 
-  sym.setToRandomSymbol()
-
+  // Main "draw" loop
   for {
-    sym.render(s, style, 120)
-    sym.rain(y)
-
+    for _, stream := range streams {
+      stream.render(s, style)
+    }
+    s.Sync()
+    time.Sleep(time.Millisecond * 100)
+    s.Clear()
   }
 }
